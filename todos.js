ada1c893d4422bc12e6deb4844aaa37afbbabb3b
Ashish Dogra <ashishdogra1697@gmail.comgit config --global user.name Ashish>
2020-07-12 12:14:25 +0530

    second commit

--- todos.js
+++ todos.js
@@ -1,12 +1,20 @@
 //click on lists to remove or line thorugh
-$("li").click(function(){
+$("ul").on("click","li",function(){
    $(this).toggleClass("complete");
 });
 
 //click on x to delete todo
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
    $(this).parent().fadeOut(500, function(){
    $(this).remove();
    });
     event.stopPropagation();
+});
+// add todo through input
+$("input[type='text']").keypress(function(event){
+  if(event.which=== 13){
+    var todo= $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todo +"</li>");
+  }
 });
